9cbee291ec28c2dca5b80a6a1bccfd1cd180af98
Unknown <antoine-janvrot@hotmail.fr>
2019-08-26 21:45:32 +0200

    AllTestAreGood

--- CarbonMap/src/test/java/GameTest.java
+++ CarbonMap/src/test/java/GameTest.java
@@ -1,12 +1,31 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+
+import entities.Map;
+import entities.MapObject;
+import entities.Mountain;
+import entities.Player;
+import entities.Treasure;
+import utils.Game;
 
 public class GameTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGame() {
+		Map map = new Map(3, 4);
+		List<Player> players = Arrays.asList(new Player(1, 1, "toto", "S", "AADADAGGA", 0));
+		List<Mountain> mountains = Arrays.asList(new Mountain(1, 0), new Mountain(2, 1));
+		List<Treasure> treasures = Arrays.asList(new Treasure(0, 3, 2), new Treasure(1, 3, 3));
+		Game game = new Game(map, mountains, treasures, players);
+		
+		game.playGame();
+		List<MapObject> finalObjects = game.getGenericList();
+		
+		assertTrue(finalObjects.size() == 5);
 	}
 
 }
